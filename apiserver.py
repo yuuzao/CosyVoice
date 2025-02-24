d43e81340f8bc1f71f6f4f51e9ac3173b1ca4ade
yuuzao <sisylocke@gmail.com>
2025-02-24 17:17:31 +0800

    change api port

--- apiserver.py
+++ apiserver.py
@@ -68,4 +68,4 @@ def text_to_speech(request: TextToSpeechRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=58000)
